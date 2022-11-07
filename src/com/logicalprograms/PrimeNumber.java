4bce61276bdc161f46686545edf2039c35ed8d51
Shubham Raj <shubhamaryan805@gmail.com>
2022-11-07 21:51:26 +0530

    Added: Logic to check if a number is prime or not

---
+++ src/com/logicalprograms/PrimeNumber.java
@@ -0,0 +1,33 @@
+package com.logicalprograms;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a no. to check if it's a prime number.");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        primeChecking(number);
+    }
+
+    public static void primeChecking(int number) {
+
+        boolean isPrime = true;
+        for (int i = 2; i <= (number / 2); i++) {
+            if ((number % i) == 0) {
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime && number >1) {
+            System.out.println("It's a prime number");
+        }
+        else {
+            System.out.println("It's not a prime number");
+        }
+
+    }
+
+}
